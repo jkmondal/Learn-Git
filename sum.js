0b8932685376314f1ada15078af97c49de211549
jkmondal <mondaljyoti1210@gmail.com>
2024-05-05 18:38:40 +0530

    add function sum.js

--- sum.js
+++ sum.js
@@ -1,3 +1,14 @@
 for (let i = 1; i <= 10; i++) {
   console.log(i);
- }
+}
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+  for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrime(17)); // Output: true
